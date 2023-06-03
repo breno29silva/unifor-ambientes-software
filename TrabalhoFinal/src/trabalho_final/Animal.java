e2aa769e9b05512ee3b851873d253fc70add0599
kenji <kenjibelarmino@hotmail.com>
2023-06-03 15:48:10 -0300

    Adicionando novos métodos na classe animal

--- TrabalhoFinal/src/trabalho_final/Animal.java
+++ TrabalhoFinal/src/trabalho_final/Animal.java
@@ -1,5 +1,20 @@
 package trabalho_final;
 
 public abstract class Animal {
+	
+	private String nome;
+	
+	public Animal(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 	abstract void emitirSom();
 }
